7344a20e0ac68528a4c41a2975faf04979e816b5
lposthumanl <lposthumanl@gmail.com>
2024-11-03 19:02:47 +0300

    create phone book menu

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,24 +1,40 @@
 
 phone_book_menu = [
-    "open file",
-    "save file",
-    "show all contacts",
-    "create contact",
-    "find contact",
-    "edit contact",
-    "delete contact",
-    "exit program",
+    "Open file",
+    "Show all contacts",
+    "Exit program",
 ]
-graphics_dl = "====================="
-graphics_sl = "---------------------"
-def show_menu(menu):
+
+file_actions_menu = [
+    "Create contact",
+    "Find contact",
+    "Edit contact",
+    "Delete contact",
+    "Save file",
+]
+
+graphics_dl = "="
+graphics_sl = "-"
+main_menu_title = "Phone book menu"
+file_menu_title = "File actions"
+
+
+def show_menu(menu, menu_title):
+    menu_items = {}
     menu_number = 1
-    print(f"{graphics_dl}\n   Phone book menu \n{graphics_sl}")
-    for position in menu:
-        # position = menu[menu_number-1]
-        print(f"{menu_number} : {position}")
-        menu_number +=1
-    print(graphics_dl)
+    print(f"{graphics_dl * (len(menu_title) + 8)}\n    {menu_title} \n{graphics_sl * (len(menu_title) + 8)}")
+    for item in menu:
+        menu_items[menu_number] = item
+        menu_number += 1
+    for key, value in menu_items.items():
+        print(f"{key} : {value}")
+    print(graphics_dl * (len(menu_title) + 8), "\n")
+    return menu_items
 
 
-show_menu(phone_book_menu)
+menu_items = show_menu(phone_book_menu, main_menu_title)
+menu_item_choice = menu_items[int(input("Please select menu item. Enter a number: "))]
+if menu_item_choice == "Open file":
+    print("Opening the contacts file")
+
+
